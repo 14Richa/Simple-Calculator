29d2e2cce59ed5903139a5b1bab68e4649e27403
Richa Sharma <richasharma@Richas-MacBook-Air.local>
2018-09-15 23:53:49 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,29 @@
+
+
+var equal = 0;
+
+$(".equal").click(
+	function()
+	{
+	 // console.log(eval(frm.txt.value));
+	 frm.txt.value = eval(frm.txt.value);
+	 equal = 1;
+	}
+)
+
+$(".digit").click(
+    function() 
+    {
+       if(equal == 0) frm.txt.value = frm.txt.value + this.value;
+       else frm.txt.value = this.value;
+    }
+
+)
+
+$(".operation").click(
+    function() {
+    	frm.txt.value = frm.txt.value + this.value;
+    	equal = 0;
+    }
+)
+
